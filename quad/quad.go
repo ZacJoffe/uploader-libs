45f04151f5354b023928895e5971d58e31229db7
ZacJoffe <zacharyjoffe@gmail.com>
2019-04-09 20:59:34 -0400

    Added form writer

--- quad/quad.go
+++ quad/quad.go
@@ -1,9 +1,50 @@
 package quad
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
 	"os"
 )
 
-func uploadFile(file *os.File) {
+func UploadFile(file *os.File) (string, error) {
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
 
+	err := writer.WriteField("return_json", "true")
+	if err != nil {
+		return "", err
+	}
+
+	part, err := writer.CreateFormFile("image", file.Name())
+	if err != nil {
+		return "", err
+	}
+	io.Copy(part, file)
+
+	err = writer.Close()
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{}
+
+	request, err := http.NewRequest("POST", "https://quad.pe/api/upload", body)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Body)
+
+	// change
+	return "", nil
 }
