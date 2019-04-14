20a0479d68546a85b803215e773846786efa614d
ZacJoffe <zacharyjoffe@gmail.com>
2019-04-14 12:08:25 -0400

    Finished coding for album upload

--- imgur/imgur.go
+++ imgur/imgur.go
@@ -10,12 +10,25 @@ import (
 	"os"
 )
 
+type responseData struct {
+	Data struct {
+		ID         string `json:"id"`
+		Error      string `json:"error"`
+		Link       string `json:"link"`
+		DeleteHash string `json:"deletehash"`
+	} `json:"data"`
+	Success bool `json:"success"`
+	Status  int  `json:"status"`
+}
+
 // UploadVideo uploads a given video file to imgur
 func UploadVideo(file *os.File, clientID string) (string, error) {
-	link, err := uploadFile(file, "video", clientID)
+	respData, err := uploadFile(file, "video", clientID)
 	if err != nil {
 		return "", err
 	}
+
+	link := respData.Data.Link
 
 	// remove '.' at the end of the returned link (if it exists)
 	if link[len(link)-1] == '.' {
@@ -27,14 +40,19 @@ func UploadVideo(file *os.File, clientID string) (string, error) {
 
 // UploadImage uploads a given image file to imgur
 func UploadImage(file *os.File, clientID string) (string, error) {
-	return uploadFile(file, "image", clientID)
+	respData, err := uploadFile(file, "image", clientID)
+	if err != nil {
+		return "", err
+	}
+
+	return respData.Data.Link, nil
 }
 
 // uploadFile uploads a file (image or video) to imgur via their api
-func uploadFile(file *os.File, fileType, clientID string) (string, error) {
+func uploadFile(file *os.File, fileType, clientID string) (*responseData, error) {
 	// check if fileType parameter is valid for use
 	if fileType != "video" && fileType != "image" {
-		return "", fmt.Errorf("Error: invalid fileType")
+		return nil, fmt.Errorf("Error: invalid fileType")
 	}
 
 	// create a new body with a multipart form for the form data
@@ -45,14 +63,14 @@ func uploadFile(file *os.File, fileType, clientID string) (string, error) {
 	//fmt.Println(file.Name())
 	part, err := writer.CreateFormFile(fileType, file.Name())
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	io.Copy(part, file)
 
 	// close writer explicitly instead of with defer to properly create form
 	err = writer.Close()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	// create new http client, make POST request to upload endpoint
@@ -60,7 +78,7 @@ func uploadFile(file *os.File, fileType, clientID string) (string, error) {
 
 	request, err := http.NewRequest("POST", "https://api.imgur.com/3/upload", body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	// add auth and content-type headers
@@ -69,52 +87,60 @@ func uploadFile(file *os.File, fileType, clientID string) (string, error) {
 
 	resp, err := client.Do(request)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	defer resp.Body.Close()
 
-	// create a new struct for JSON response
-	type responseData struct {
-		Data struct {
-			Error string `json:"error"`
-			Link  string `json:"link"`
-		} `json:"data"`
-		Success bool `json:"success"`
-		Status  int  `json:"status"`
-	}
+	/*
+		// create a new struct for JSON response
+		type responseData struct {
+			Data struct {
+				Error string `json:"error"`
+				Link  string `json:"link"`
+			} `json:"data"`
+			Success bool `json:"success"`
+			Status  int  `json:"status"`
+		}
+	*/
 
 	var data responseData
 
 	// decode JSON response data into new instance of the struct
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	// if unsuccessful, return the http error code and the error message
 	if data.Success == false {
-		return "", fmt.Errorf("%d Error: %s", data.Status, data.Data.Error)
+		return nil, fmt.Errorf("%d Error: %s", data.Status, data.Data.Error)
 	}
 
 	// successful request, return the link to the newly uploaded file
-	return data.Data.Link, nil
+	//return data.Data.Link, nil
+	return &data, nil
 }
 
-func UploadImages(files []*os.Files, clientID string) (string, error) {
+func UploadImages(files []*os.File, clientID string) (string, error) {
 	body := new(bytes.Buffer)
 	writer := multipart.NewWriter(body)
 
 	for _, file := range files {
-		// upload file, get delete hash
-		deleteHash := "test"
-		err := writer.WriteField("deletehashes[]", deleteHash)
+		// upload file , get delete hash
+		respData, err := uploadFile(file, "image", clientID)
+		if err != nil {
+			return "", err
+		}
+		deleteHash := respData.Data.DeleteHash
+
+		err = writer.WriteField("deletehashes[]", deleteHash)
 		if err != nil {
 			return "", nil
 		}
 	}
 
-	err = writer.Close()
+	err := writer.Close()
 	if err != nil {
 		return "", err
 	}
@@ -137,15 +163,16 @@ func UploadImages(files []*os.Files, clientID string) (string, error) {
 
 	defer resp.Body.Close()
 
-	type responseData struct {
+	type albumResponseData struct {
 		Data struct {
-			ID string `json:"id"`
+			ID    string `json:"id"`
+			Error string `json:"error"`
 		} `json:"data"`
 		Success bool `json:"success"`
 		Status  int  `json:"status"`
 	}
 
-	var data responseData
+	var data albumResponseData
 
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
